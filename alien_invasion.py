e9b939c7230292818a8eea13be0b1351de9d55d4
tuboshuer <tuboshuer@yyds.com>
2025-08-25 14:43:03 +0800

    增加了小飞机左右移动

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 class AlienInvasion:
     """管理游戏资源和行为的类"""
@@ -9,25 +10,69 @@ class AlienInvasion:
         pygame.init()
         self.clock = pygame.time.Clock()
         self.settings = Settings()
+        self.bg_color = (self.settings.bg_color)
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        self.ship = Ship(self)
         pygame.display.set_caption("Alien Invasion")
-
-        #设置背景色
-        self.bg_color = (230,230,230)
 
     def run_game(self):
         """start game"""
         while True:
             #监听键盘和鼠标事件
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
+            self._check_events()
+            self.ship.update()
             #每次循环时都重新绘制屏幕
-            self.screen.fill(self.settings.bg_color)
-            #让最近绘制的屏幕可见
-            pygame.display.flip()
+            self._update_screen()
             #确保这个循环每秒运行60次，tick参数为帧率
             self.clock.tick(60)
+
+
+    def _check_keydown_events(self,event):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    #向右移动飞船
+                    self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+    def _check_keyup_events(self,event):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYUP:
+                if event.key ==pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False     
+            
+    def _check_events(self):
+        '''响应按键和鼠标事件'''
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    #向右移动飞船
+                    self.ship.moving_right = True
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+            elif event.type == pygame.KEYUP:
+                if event.key ==pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False     
+            
+
+
+
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitem()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- ship.py
+++ ship.py
@@ -4,3 +4,25 @@ class Ship:
     def __init__(self,ai_game):
         """初始化飞船并设置其初始位置"""
         self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
+
+        #加载飞船图像并获取其外接矩形
+        self.image = pygame.image.load('imges/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        #每艘飞船都放在屏幕底部的中央
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.x = float(self.rect.x)
+        self.moving_right = False
+        self.moving_left = False
+    def update(self):
+        """根据移动标志调整飞机位置"""
+        if self.moving_right and self.rect.right<self.screen_rect.right:
+            self.x += self.settings.ship_speed    
+        if self.moving_left and self.rect.left>self.screen_rect.left:
+            self.x -=self.settings.ship_speed
+        self.rect.x = self.x
+    def blitem(self):
+        """在指定位置绘制飞船"""
+        self.screen.blit(self.image,self.rect)
